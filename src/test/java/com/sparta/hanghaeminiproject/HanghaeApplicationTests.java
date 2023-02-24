ab5d09b5544b5dafafd870c080bc189c0bc712ba
DESKTOP-380TO0D\ktc system <ledgy98@gmail.com>
2023-02-24 18:11:08 +0900

    make: setting

---
+++ src/test/java/com/sparta/hanghaeminiproject/HanghaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaeminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
